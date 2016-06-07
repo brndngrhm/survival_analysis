dcbbfdcb30cbbe70d8990b04c108d6ae09348060
brndngrhm <brendanvgraham@gmail.com>
2016-06-07 09:19:32 -0400

    week 1 examples / loading data + packages

---
+++ class ex/week_1.R
@@ -0,0 +1,9 @@
+library(survival)
+library(nlme)
+
+aids <- read.delim("C:/Users/GRA/Desktop/Misc/R Working Directory/School/survival_analysis/data/aids.txt")
+divorce <- read.delim("C:/Users/GRA/Desktop/Misc/R Working Directory/School/survival_analysis/data/divorce.txt")
+gbcs <- read.delim("C:/Users/GRA/Desktop/Misc/R Working Directory/School/survival_analysis/data/gbcs.txt")
+lung <- read.delim("C:/Users/GRA/Desktop/Misc/R Working Directory/School/survival_analysis/data/lungcancer.txt")
+uis <- read.delim("C:/Users/GRA/Desktop/Misc/R Working Directory/School/survival_analysis/data/uis.txt")
+whas500 <- read.delim("C:/Users/GRA/Desktop/Misc/R Working Directory/School/survival_analysis/data/whas500.txt")
