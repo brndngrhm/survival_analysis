c556c7f40d356ee6a7c691b8f82fe9d460df68ac
brndngrhm <brendanvgraham@gmail.com>
2016-06-07 13:31:03 -0400

    added example for notes

--- class_ex/week_1.R
+++ class_ex/week_1.R
@@ -22,7 +22,7 @@ lungsurv <- Surv(lung$time, lung$status-1) # originally coded as 1,2. This recod
 fit <- survfit(lungsurv~1)
 
 #plot the survival curve
-plot(fit,  conf.int= T, mark.time=F, 
+plot(fit,  conf.int= T, mark.time=T, 
      main = "Survival Curve for Lung Cancer Data", 
      xlab = "Time", ylab = "Survival Rate")
 
@@ -34,5 +34,24 @@ plot(fit2, conf.int= T, mark.time=F,
      xlab = "Time", ylab = "Survival Rate",
      ylim=c(.5,1))
 
-
 #life table example ----
+
+# for notes ----
+data <- data.frame(cbind(c(4, 6, 14, 20, 22), c(1,1,1,1,1)))
+names(data)[1] <- "data"
+names(data)[2] <- "status"
+data.surv <- Surv(data$data, data$status)
+fit <- survfit(data.surv ~1)
+plot(fit, conf.int= F,
+     main = "Survival Curve for Class Example", 
+     xlab = "Time", ylab = "Survival Rate")
+
+data <- data.frame(cbind(c(4, 6, 14, 16, 20, 22, 24, 40), c(1,1,1,0,1,1,0,1)))
+names(data)[1] <- "data"
+names(data)[2] <- "status"
+data.surv <- Surv(data$data, data$status)
+fit <- survfit(data.surv ~1)
+plot(fit, conf.int= F, mark.time = T, 
+     main = "Survival Curve for Class Example", 
+     xlab = "Time", ylab = "Survival Rate")
+
