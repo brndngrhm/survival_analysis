f1ec7b6930c4c49e0a4cc5dab32d93c79a1af377
brndngrhm <brendanvgraham@gmail.com>
2016-06-14 11:11:24 -0400

    added summary plots

--- project/data_import.R
+++ project/data_import.R
@@ -20,6 +20,24 @@ ep <- deaths %>% group_by(episode) %>% summarise(total = sum(murdered))
 season <- deaths %>% group_by(season) %>% summarise(total = sum(murdered))
 house <- deaths %>% group_by(house) %>% summarise(total = sum(murdered)) %>% ungroup() %>% arrange(desc(total))
 type <-  deaths %>% group_by(type) %>% summarise(total = sum(murdered)) %>% ungroup() %>% arrange(desc(total))
+censored <- deaths %>% group_by(murdered) %>% summarise(total = n()) %>% ungroup() %>% arrange(desc(total))
+
+#summary plots
+(season.plot <- ggplot(season, aes(x=as.factor(season), y=total)) + 
+  geom_bar(stat="identity") + theme_hc()+ 
+  labs(x="\n Season", y="", title="Total Murders by Season"))
+
+(ep.plot <- ggplot(ep, aes(x=as.factor(episode), y=total)) + 
+  geom_bar(stat="identity") + theme_hc() + 
+  labs(x="\nEpisode", y="", title="Total Murders by Episode Number"))
+
+(house.plot <- ggplot(house, aes(x=reorder(as.factor(house), total), y=total)) + 
+  geom_bar(stat="identity") + coord_flip() + theme_hc() + 
+  labs(x="\nHouse", y="", title="Total Murders by House"))
+
+(type.plot <- ggplot(type, aes(x=reorder(as.factor(type), -total), y=total)) + 
+  geom_bar(stat="identity")+ theme_hc() + 
+  labs(x="\nCharacter Role", y="", title="Total Murders by Role"))
 
 #plot simple survival curve
 death.surv <- Surv(deaths$time, deaths$murdered)
